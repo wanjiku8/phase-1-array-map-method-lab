b75d293dfe926846581c4e82f3b3360689e150a3
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-09 22:29:50 +0300

    commit

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials.map(function(title) {
-    return title
-      .split(' ') // Split each title into words
-      .map(function(word) {
-        return word.charAt(0).toUpperCase() + word.slice(1); // Capitalize the first letter
-      })
-      .join(' '); // Join the words back into a string
+function titleCased() {
+  return tutorials.map((tutorial) => {
+    return tutorial
+      .split(" ") 
+      .map(
+        (word) =>
+          word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() // Capitalize the first letter of each word
+      )
+      .join(" "); 
   });
 }
 
